50decf55ab98c6446335062751bde64234df0ad9
Alejandro Alomia <alejandroalomia730@gmail.com>
2022-12-28 02:31:51 -0500

    v 0.0.2

--- card.go
+++ card.go
@@ -7,30 +7,57 @@ import (
 	"unicode/utf8"
 )
 
-func Type(cardNumber string) string {
-	prefix, _ := strconv.Atoi(cardNumber[:2])
+type card struct {
+	number string
+}
+
+func New(number string) card {
+	return card{
+		number: number,
+	}
+}
+
+func (c card) Details() {
+	var information strings.Builder
+
+	information.WriteString(fmt.Sprintf("Card Number:\t%s\n", c.number))
+	information.WriteString(fmt.Sprintf("Card Type:\t%s\n", c.Network()))
+	information.WriteString(fmt.Sprintf("Card Length:\t%d\n", c.length()))
+	information.WriteString(fmt.Sprintf("Card Valid:\t%v", c.IsValid()))
+
+	fmt.Println(information.String())
+}
+
+func (c card) IsValid() bool {
+	return c.luhnAlgorithm()
+}
+
+func (c card) Network() string {
+	prefix, _ := strconv.Atoi(c.number[:2])
 
 	switch prefix {
-		case 34 , 37:
-			return "American Express"
-		case 51 , 52 , 53 , 54 , 55:
-			return "MasterCard"
-		case 40, 41, 42, 43, 44, 45, 46, 47, 48, 49:
-			return "Visa"
+	case 34, 37:
+		return "American Express"
+	case 51, 52, 53, 54, 55:
+		return "MasterCard"
+	case 40, 41, 42, 43, 44, 45, 46, 47, 48, 49:
+		return "Visa"
 	}
 	return "unknown"
 }
 
-func Length(cardNumber string) int {
-	return utf8.RuneCountInString(strings.Replace(cardNumber, " ", "", -1))
+func (c card) length() int {
+	return utf8.RuneCountInString(strings.Replace(c.number, " ", "", -1))
 }
 
-func luhnAlgorithm(cardNumber string) bool {
+func (c card) luhnAlgorithm() bool {
 	var firstSum int
 	var secondSum int
 
-	for i := Length(cardNumber) -2; i >= 0; i -= 2 {
-		n, _ := strconv.Atoi(string(cardNumber[i]))
+	cc := strings.Replace(c.number, " ", "", -1)
+
+	for i := c.length() - 2; i >= 0; i -= 2 {
+		n, _ := strconv.Atoi(string(cc[i]))
 		nd := n * 2
 
 		if nd > 9 {
@@ -41,25 +68,10 @@ func luhnAlgorithm(cardNumber string) bool {
 		}
 	}
 
-	for i := 1; i < Length(cardNumber); i += 2 {
-		n, _ := strconv.Atoi(string(cardNumber[i]))
+	for i := 1; i < c.length(); i += 2 {
+		n, _ := strconv.Atoi(string(cc[i]))
 		secondSum += n
 	}
 
-	return (firstSum + secondSum) % 10 == 0
-}
-
-func IsValid(cardNumber string) bool {
-	return luhnAlgorithm(strings.Replace(cardNumber, " ", "", -1))
-}
-
-func Details(cardNumber string) {
-	var information strings.Builder
-
-	information.WriteString(fmt.Sprintf("Card Number:\t%s\n",	cardNumber))
-	information.WriteString(fmt.Sprintf("Card Type:\t%s\n", 	Type(cardNumber)))
-	information.WriteString(fmt.Sprintf("Card Length:\t%d\n",	Length(cardNumber)))
-	information.WriteString(fmt.Sprintf("Card Valid:\t%v",		IsValid(cardNumber)))
-
-	fmt.Println(information.String())
+	return (firstSum+secondSum)%10 == 0
 }
